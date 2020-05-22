dc82a7f4726b3ca4c4ff313af54d713277191151
budostylz <lewis.shaun1@outlook.com>
2020-05-22 15:49:03 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import logo from './logo.svg';
+import Movies from './Movies';
 
 /*
 Use React and the data below to display a list of users alongside their favorite movies.
@@ -106,6 +107,11 @@ class App extends Component {
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
         <h2>Favorite Movies</h2>
+		<Movies profiles={profiles} users ={users} movies={movies} /> 
+
+      
+		
+		
       </div>
     );
   }
---
+++ src/Movies.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+
+
+  /*
+  
+  
+  Old Solution 
+  Return in App.js <Movies profiles={profiles} users ={users} movies={movies} /> 
+  
+  */
+class Movies extends Component {
+  render(){
+    
+
+
+
+ this.props.profiles.map((profile) => {
+    let user = this.props.users[profile.id].name;
+    let movie = this.props.movies[profile.favoriteMovieID].name;
+
+   //`${userName}\'s favorite movie is "${favMovieName}."`
+    //console.log(`${user}\'s favorite movie is "${movie}."` );
+    
+
+
+
+  })
+    
+ 
+
+
+    
+    return(
+      
+    	<ol>
+      
+        {
+          
+          this.props.profiles.map((profile) => {
+          let user = this.props.users[profile.id].name;
+          let movie = this.props.movies[profile.favoriteMovieID].name;
+
+          //`${userName}\'s favorite movie is "${favMovieName}."`
+          //console.log(`${user}\'s favorite movie is "${movie}."` );
+      
+      		return(
+              <li key={profile.id}> {`${user}\'s favorite movie is "${movie}."`} </li>
+            
+            )
+      		
+
+        })
+
+
+        }
+
+        </ol>
+    
+    
+    );
+
+    
+  }
+  
+}//Movies
+
+export default Movies;
